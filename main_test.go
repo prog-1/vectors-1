154ab98bbdedb66a1b1a97ac83a68e690e7da829
Aleksandrs Stukalovs <astukalovs@edu.riga.lv>
2023-02-03 08:42:43 +0200

    Added tests

--- main_test.go
+++ main_test.go
@@ -3,13 +3,17 @@ package main
 import "testing"
 
 func TestIncludesPoint(t *testing.T) {
-	for tc := range []struct {
+	for _, tc := range []struct {
 		l    Line
 		p    Vector
 		want bool
 	}{
-		{Line{Vect{2, 5}, {6, -2}}, {8, 3}, true},
+		{Line{Vector{2, 5}, Vector{6, -2}}, Vector{8, 3}, true},
+		{Line{Vector{2, 5}, Vector{6, -2}}, Vector{5, 2}, false},
+		{Line{Vector{2, 5}, Vector{6, -2}}, Vector{11, 2}, true},
 	} {
-
+		if got := tc.l.IncludesPoint(tc.p); got != tc.want {
+			t.Errorf("got = %v, want = %v", got, tc.want)
+		}
 	}
 }
